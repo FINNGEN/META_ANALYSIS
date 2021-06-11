cf7acb6e58764dbdd01d7f7565eea70ea471f6de
juhaa <juha.mehtonen@outlook.com>
2021-06-11 17:53:29 +0300

    Simplify

--- scripts/create_meta_confs_fg_est.py
+++ scripts/create_meta_confs_fg_est.py
@@ -23,29 +23,29 @@ def generate_json(mapping):
         'extra_cols': ['af_alt', 'af_alt_cases', 'af_alt_controls', 'nearest_genes']
     }
 
-    ukbb_conf = {
-        'name': 'UKBB',
-        'file': mapping['ukbb_link'],
-        'n_cases': int(mapping['ukbb_n_cases']),
-        'n_controls': int(mapping['ukbb_n_controls']),
-        'chr': '#chr',
-        'pos': 'pos',
-        'ref': 'ref',
-        'alt': 'alt',
-        'effect': 'beta_EUR',
+    est_conf = {
+        'name': 'ESTBB',
+        'file': mapping['estbb_link'],
+        'n_cases': int(mapping['estbb_n_cases']),
+        'n_controls': int(mapping['estbb_n_controls']),
+        'chr': '#CHR',
+        'pos': 'POS',
+        'ref': 'REF',
+        'alt': 'ALT',
+        'effect': 'beta',
         'effect_type': 'beta',
-        'pval': 'pval_EUR',
-        'se': 'se_EUR',
-        'extra_cols': ['low_confidence_EUR']
+        'pval': 'pval',
+        'se': 'sebeta',
+        'extra_cols': ["af_alt", "AF.Cases", "AF.Controls"]
     }
 
     with open(mapping['fg_phenotype'] + '.json', 'w') as out:
-        json.dump({'meta': [fg_conf, ukbb_conf]}, out, indent=4)
+        json.dump({'meta': [fg_conf, est_conf]}, out, indent=4)
 
 
 def run():
 
-    parser = argparse.ArgumentParser(description='Create a json config file for meta-analysis combining results from FinnGen, and UKBB')
+    parser = argparse.ArgumentParser(description='Create a json config file for meta-analysis combining results from FinnGen, and EST')
     parser.add_argument('in_mapping_file', action='store', type=str, help='A tab-delimited text file with phenotype mapping information between studies')
 
     args = parser.parse_args()
@@ -53,14 +53,14 @@ def run():
     # Read mapping and drop rows with missing values (missing pair)
     mapping = pd.read_csv(args.in_mapping_file, sep='\t', dtype=object)
     mapping.dropna(axis=0, how='any', inplace=True)
-    #mapping.index = pd.MultiIndex.from_frame(mapping[['fg_phenotype', 'ukbb_phenotype', 'est_phenotype']])
+    #mapping.index = pd.MultiIndex.from_frame(mapping[['fg_phenotype', 'est_phenotype']])
 
     # Print summary stat links to file
-    mapping[['fg_link', 'ukbb_link']].to_csv('sumstat_files.txt', header=False, index=False, sep='\t')
+    mapping[['fg_link', 'estbb_link']].to_csv('sumstat_files.txt', header=False, index=False, sep='\t')
 
     # Replace gs prefix with cromwell_root for cromwell run
     mapping['fg_link'] = mapping['fg_link'].replace('^gs:/', '/cromwell_root', regex=True)
-    mapping['ukbb_link'] = mapping['ukbb_link'].replace('^gs:/', '/cromwell_root', regex=True)
+    mapping['estbb_link'] = mapping['estbb_link'].replace('^gs:/', '/cromwell_root', regex=True)
 
     # Generate json configs for meta-analysis
     mapping.apply(generate_json, axis=1)
